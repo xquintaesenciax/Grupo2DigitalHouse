85d61ab6475493096dfea78bc05ba3a518f72436
Luciano <ocampoluciano123@gmail.com>
2024-03-12 21:46:01 -0300

    Agregado de loading

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   development: {
-    username: "root",
-    password: null,
-    database: "velvet_2",
-    host: "127.0.0.1",
+    username: "un67xs8a5qfboqxs",
+    password: "moe4OTBmU5wp015nOHw2",
+    database: "bizzo4kzs2obssnnxoqz",
+    host: "bizzo4kzs2obssnnxoqz-mysql.services.clever-cloud.com",
     dialect: "mysql",
   },
   test: {
